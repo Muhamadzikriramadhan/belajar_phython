fc85dc14013810d554313453d1f7367f839652a3
zikri29 <ramadhanmzikri@gmail.com>
2021-04-19 09:43:40 +0700

    push #1

---
+++ bp1.py
@@ -0,0 +1,13 @@
+name = 'Muhamad Zikri Ramadhan'
+age = 20
+
+
+istri = str(input('Nama Ummi = '))
+
+print('Hello, My name is ' + str(name) + ', My Age is ' + str(age) + ' Years Old. i am is Abi ' + str(name))
+
+print('Ummi nya Abi Zikri Yaitu Ummi ' + istri +'.')
+
+anak = str(input('Nama Anak = '))
+
+print('Anak Abi dari ' + name + ' dan Ummi ' + istri + ', dan Anaknya Adalah ' + anak )
---
+++ bp2.py
@@ -0,0 +1,20 @@
+warna = [["Merah", 34], ["Kuning", 45], ["Hijau", 34]]
+print (warna)
+
+keluarga = ["irin", 4, "agus", 5, "sena", 7, "adi", 5]
+print (keluarga)
+
+hasil = (keluarga[1] * keluarga[3] / keluarga[7] + keluarga[5])
+print (hasil)
+
+keluarga[2] = "Bang Jago"
+print (keluarga)
+
+del(keluarga[2])
+print (keluarga)
+
+keluarga[2] = "Agustian"
+print (keluarga)
+
+keluarga = ["irin", 4, "agus", 5, "sena", 7, "adi", 5]
+print (keluarga)
